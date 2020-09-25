27c00ca08aa9001d9be6b38f2fa5ee6a4185f017
Arthur Carneiro <arthur.carneirontk@gmail.com>
2020-09-25 00:50:08 -0300

    Deleting itens

--- js/editUser.js
+++
@@ -1,8 +0,0 @@
-function onEdit(td) {
-  console.log('editando');
-  selectedRow = td.parentElement.parentElement;
-  document.getElementById('name').value = selectedRow.cells[0].innerHTML;
-  document.getElementById('email').value = selectedRow.cells[1].innerHTML;
-  document.getElementById('tel').value = selectedRow.cells[2].innerHTML;
-  document.getElementById('city').value = selectedRow.cells[3].innerHTML;
-}
--- js/main.js
+++ js/main.js
@@ -11,6 +11,50 @@ function allStorage() {
   return archive;
 }
 
+function editUser(index) {
+  console.log('deu', index);
+  const editUser = $(document).ready(function () {
+    $('.table-content').click(() => {
+      const nameArray = sessionStorage.getItem('name');
+      const emailArray = sessionStorage.getItem('email');
+      const telArray = sessionStorage.getItem('tel');
 
+      const name = $('#name').val();
+      const email = $('#email').val();
+      const tel = $('#tel').val();
+
+      let names = [];
+      let emails = [];
+      let tels = [];
+
+      names = [...JSON.parse(nameArray), name];
+      emails = [...JSON.parse(emailArray), email];
+      tels = [...JSON.parse(telArray), tel];
+      console.log('names', names);
+    });
+  });
+
+  return editUser;
+}
+
+function deleteUser(index) {
+  console.log('deu', index);
+  const editUser = $(document).ready(function () {
+    $('.table-content').click(() => {
+      const nameArray = sessionStorage.getItem('name');
+      const emailArray = sessionStorage.getItem('email');
+      const telArray = sessionStorage.getItem('tel');
+
+      const teste = JSON.parse(nameArray);
+
+      console.log(teste[index]);
+      teste.splice(index, 1);
+      sessionStorage.setItem('name', JSON.stringify(teste));
+      console.log('names', names);
+    });
+  });
+  document.location.reload();
+  return editUser;
+}
 
 allStorage();
